a78ce3201b727fd1eecef34bce72b664f683c1ce
yahyaatuom <yahya.khan@uom.edu.pk>
2026-05-23 23:42:13 +0500

    Position change

--- compute_NDVI.py
+++ compute_NDVI.py
@@ -1,3 +1,5 @@
+#Compute NDVI - NumPy operates element-wise on entire arrays
+#np.where avoids division by zero
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,8 +12,6 @@ red = np.random.uniform(0.05, 0.2, (height, width))
 nir[20:30, 20:30] = np.random.uniform(0.1, 0.2, (10, 10))  # low NIR
 red[20:30, 20:30] = np.random.uniform(0.3, 0.5, (10, 10))  # high Red
 
-#Compute NDVI - NumPy operates element-wise on entire arrays
-#np.where avoids division by zero
 
 ndvi = np.where(
     (nir+red) ==0,

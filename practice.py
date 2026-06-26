aead4401d2f3ecc18de885e2e85e4d22184d866d
yahyaatuom <yahya.khan@uom.edu.pk>
2026-06-26 21:58:36 +0400

    has to rename again

---
+++ practice.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.zeros((3,4))
+print(a)
+
+b = np.ones((2,3,4), dtype =np.int16)
+print(b)
+
+c = np.empty((2,3))
+print(c)
+
+d = np.arange(10,30,5)
+print(d)
+
+e = np.arange(0,2,0.3)
+print(e)

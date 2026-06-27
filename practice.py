f23b6812f94ba942d6de4ee0ef1f42fe4d639db4
yahyaatuom <yahya.khan@uom.edu.pk>
2026-06-27 15:49:06 +0400

    Histograms

--- practice.py
+++ practice.py
@@ -1,20 +1,10 @@
 import numpy as np
+rg = np.random.default_rng(1)
 import matplotlib.pyplot as plt
-def mandelbrot(h, w, maxit=20, r=2):
-    """Returns an image of the Mandelbrot fractal of size (h,w)."""
-    x = np.linspace(-2.5, 1.5, 4*h+1)
-    y = np.linspace(-1.5, 1.5, 3*w+1)
-    A, B = np.meshgrid(x, y)
-    C = A + B*1j
-    z = np.zeros_like(C)
-    divtime = maxit + np.zeros(z.shape, dtype=int)
-    for i in range(maxit):
-        z = z**2 + C
-        diverge = abs(z) > r                    # who is diverging
-        div_now = diverge & (divtime == maxit)  # who is diverging now
-        divtime[div_now] = i                    # note when
-        z[diverge] = r                          # avoid diverging too much
-    return divtime
-plt.clf()
-plt.imshow(mandelbrot(400, 400))
+mu, sigma = 2, 0.5
+v = rg.normal(mu, sigma, 10000)
+plt.hist(v, bins=50, density=True)      
+(n, bins) = np.histogram(v, bins=50, density=True) 
+plt.plot(.5 * (bins[1:] + bins[:-1]), n) 
+
 plt.show()

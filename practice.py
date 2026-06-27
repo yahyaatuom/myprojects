f01a6d09fe78ee5e871761a5084287e09e87406c
yahyaatuom <yahya.khan@uom.edu.pk>
2026-06-27 13:54:48 +0400

    working

--- practice.py
+++ practice.py
@@ -1,16 +1,12 @@
 import numpy as np
 
-a = np.zeros((3,4))
-print(a)
+B=np.arange(3)
+print(B)
 
-b = np.ones((2,3,4), dtype =np.int16)
-print(b)
+print(np.exp(B))
 
-c = np.empty((2,3))
-print(c)
+print(np.sqrt(B))
 
-d = np.arange(10,30,5)
-print(d)
+C = np.array([2., -1., 4.])
 
-e = np.arange(0,2,0.3)
-print(e)
+print(np.add(B,C))

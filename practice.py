369cbfe908e88ba26fabf1d799b19fe1a438f603
yahyaatuom <yahya.khan@uom.edu.pk>
2026-06-27 15:35:02 +0400

    Mandelbrot fractal

--- practice.py
+++ practice.py
@@ -1,29 +1,20 @@
 import numpy as np
-from numpy import newaxis
-
-
-rg = np.random.default_rng()
-
-a = np.array([[0,1,2,3],
-              [4,5,6,7],
-              [8,9,10,11]])
-
-# b = a
-# print(b is a)
-
-# def f(x):
-#     print(id(x))
-
-# print(id(a))
-# f(a)
-
-c=a.view()
-# print(c)
-# print(c is a)
-
-# print(c.base is a) # c is a view of the data owned by a
-
-# print(c.flags.owndata)
-
-c = c.reshape((2,6)) # this will have no effect on a's shape
-print(a.shape)
+import matplotlib.pyplot as plt
+def mandelbrot(h, w, maxit=20, r=2):
+    """Returns an image of the Mandelbrot fractal of size (h,w)."""
+    x = np.linspace(-2.5, 1.5, 4*h+1)
+    y = np.linspace(-1.5, 1.5, 3*w+1)
+    A, B = np.meshgrid(x, y)
+    C = A + B*1j
+    z = np.zeros_like(C)
+    divtime = maxit + np.zeros(z.shape, dtype=int)
+    for i in range(maxit):
+        z = z**2 + C
+        diverge = abs(z) > r                    # who is diverging
+        div_now = diverge & (divtime == maxit)  # who is diverging now
+        divtime[div_now] = i                    # note when
+        z[diverge] = r                          # avoid diverging too much
+    return divtime
+plt.clf()
+plt.imshow(mandelbrot(400, 400))
+plt.show()

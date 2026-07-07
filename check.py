64ee0286d3f892694cba058acca552cbe97289f6
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-07 18:39:40 +0400

    Slight change

--- check.py
+++ check.py
@@ -2,5 +2,7 @@
 from height import feet_to_inch
 
 height = float(input("Enter your height in feet: "))
+if height <= 0:
+    print("Height cannot be 0 or a negative value")
 height_in_inches = feet_to_inch(height)
 print(height_in_inches)

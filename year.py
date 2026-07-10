a88ace61495d96a1d8cca138fa1a299d6a88f41d
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-10 18:16:31 +0400

    we'll write it to find your current age

--- year.py
+++
@@ -1,9 +0,0 @@
-def age_in_2070():
-    age = input("Enter your age: ")
-    current_year = 2023
-    years_to_2070 = 2070 - current_year
-    age_in_2070 = int(age) + years_to_2070
-    return age_in_2070
-
-
-

580cc4882fce3c10dac6fa79c3f832511a3f3834
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-12 13:59:11 +0400

    wprking..

--- car_price_predictor.py
+++ car_price_predictor.py
@@ -17,3 +17,14 @@ p_data = data.dropna()
 p_data.isnull().sum()
 
 p_data.info()
+p_data.fuel.value_counts()
+p_data.seller_type.value_counts()
+p_data.transmission.value_counts()
+p_data.owner.value_counts()
+
+p_data.replace({'fuel':{'Petrol':0,'Diesel':1,'CNG':2,'LPG':3,'Electric':4}},inplace=True)
+p_data.replace({'seller_type':{'Individual':0,'Dealer':1,'Trustmark Dealer':2}},inplace=True)
+p_data.replace({'transmission':{'Manual':0,'Automatic':1}},inplace=True)
+p_data.replace({'owner':{'First Owner':0,'Second Owner':1,'Third Owner':2,'Fourth & Above Owner':3,'Test Drive Car':4}},inplace=True)
+
+p_data.head()

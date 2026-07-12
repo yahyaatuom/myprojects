27c59746aa6c13e9695964ea2208d2c1ade24900
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-12 14:27:56 +0400

    done

--- car_price_predictor.py
+++ car_price_predictor.py
@@ -28,3 +28,26 @@ p_data.replace({'transmission':{'Manual':0,'Automatic':1}},inplace=True)
 p_data.replace({'owner':{'First Owner':0,'Second Owner':1,'Third Owner':2,'Fourth & Above Owner':3,'Test Drive Car':4}},inplace=True)
 
 p_data.head()
+p_data.corr(numeric_only = True)
+
+x = p_data.drop(['name','selling_price','owner'], axis = 1)
+y = p_data['selling_price']
+
+print(x)
+print(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state=1)
+
+#Training the Model
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+
+predicted_data = lr.predict(x_train)
+r2 = metrics.r2_score(y_train, predicted_data)
+print("R squared error :", r2)
+
+plt.scatter(y_train, predicted_data)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Price")
+plt.title(" Actual Prices vs Predicted Prices")
+plt.show()

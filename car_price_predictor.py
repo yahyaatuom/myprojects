004adad5358fb1ec4f992dc69f70254483168ad4
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-12 13:54:45 +0400

    Imported the required libraries

---
+++ car_price_predictor.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+from sklearn.linear_model import SGDRegressor
+from sklearn import metrics
+

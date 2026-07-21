8c5e27c24000e7835a1be5d67e9f34be65b8ce3b
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-21 21:39:13 +0400

    plot

--- home_price_predictor.py
+++ home_price_predictor.py
@@ -8,25 +8,29 @@ from sklearn import linear_model  # The most important library here
 df = pd.read_csv("houses.csv", sep="\t")
 #print(df)
 
-plt.scatter(df.area, df.price, color = "red", marker = "+")
-plt.xlabel("Area (Sq.Ft)")
-plt.ylabel("Price(PKR)")
+# plt.scatter(df.area, df.price, color = "red", marker = "+")
+# plt.xlabel("Area (Sq.Ft)")
+# plt.ylabel("Price(PKR)")
 
-#Training the Model
+# #Training the Model
 
 reg = linear_model.LinearRegression()
 reg.fit(df[["area"]], df.price)
 
-print(reg.predict([[50000]]))
+# print(reg.predict([[50000]]))
 
 #print(reg.coef_)
 #print(reg.intercept_)
 
-print(7481.08108108*5000+-6867837.837837841)
+#print(7481.08108108*5000+-6867837.837837841)
+plt.xlabel('area', fontsize=20)
+plt.ylabel('price',fontsize=20)
+plt.scatter(df.area,df.price,color='red',marker='+')
+plt.plot(df.area, reg.predict(df[['area']]), color = 'blue')
+plt.show()
+# d = pd.read_csv("area.csv")
+# d.head(3)
 
-d = pd.read_csv("area.csv")
-d.head(3)
-
-p = reg.predict(d)
-d['prices'] = p
-d.to_csv('prediction.csv',index=False)
+# p = reg.predict(d)
+# d['prices'] = p
+# d.to_csv('prediction.csv',index=False)

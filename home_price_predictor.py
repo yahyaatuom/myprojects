80b3322cd6b75642e77695fadcfd67528abb595f
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-21 17:22:26 +0400

    Imported libraries

--- home_price_predictor.py
+++ home_price_predictor.py
@@ -1,1 +1,6 @@
 #Simple Linear Regression model that'd predict home prices in NJ, USA
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model  # The most important library here

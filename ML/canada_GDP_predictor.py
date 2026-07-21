2239b85cd8b3e3090838ba1018c4cfb71ee4add3
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-21 21:54:20 +0400

    done:

--- ML/canada_GDP_predictor.py
+++ ML/canada_GDP_predictor.py
@@ -1,10 +1,14 @@
-#This Program will train a model using the records in csv optimizing the Linear Regression Algorithm to Predict the Per Capita Income of Canada in The Year 2020
-
+# This Program will train a model using the records in csv optimizing the Linear Regression Algorithm to Predict the Per Capita Income of Canada in The Year 2020
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn import linear_model
 
+# Load dataset
 df = pd.read_csv("ML/canada_per_capita_income.csv")
 
-print(df)
+reg = linear_model.LinearRegression()
+reg.fit(df[["year"]], df["per capita income (US$)"])
+
+predicted_income = reg.predict([[2020]])
+print(predicted_income)

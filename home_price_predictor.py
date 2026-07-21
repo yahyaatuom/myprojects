1e76b4f449480ea03d9400a63167fc4f1b631985
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-21 17:27:43 +0400

    House values added adn imported

--- home_price_predictor.py
+++ home_price_predictor.py
@@ -4,3 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model  # The most important library here
+
+df = pd.read_csv("houses.csv")
+print(df)

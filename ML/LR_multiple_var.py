b3382cf4ad200eac91e10ef271fca82410b098cd
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 00:00:47 +0400

    dataset added

--- ML/LR_multiple_var.py
+++ ML/LR_multiple_var.py
@@ -1,1 +1,8 @@
 # Writing a program, again optimizing the Linear Regression Algorithm in order to predict prices of houses(New Jersey, USA), this time using more than one variable
+
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+
+df = pd.read_csv('ML/house_var.csv')
+print(df)

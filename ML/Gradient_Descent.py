b39dc58d20a3460898a45d328d1826d7c0d50fb4
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 15:37:01 +0400

    Learning approach

--- ML/Gradient_Descent.py
+++ ML/Gradient_Descent.py
@@ -4,3 +4,6 @@
 #MSE = 1/n sigma (yi - (mxi+b))^2
 #We cannot take each and every line in a massive dataset (not a smart approach), hence we use grdient descent
 #GD is an algorithm that finds best fit line for given training data set
+
+# 2 approaches 1. fixed steps, 2. steps that follow the curvature
+#approach 2 is better, to implement we have to calculate slope at each step

789e37b51407b18fdf656c978dd78e525685fe2b
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 15:32:09 +0400

    MSE

--- ML/Gradient_Descent.py
+++ ML/Gradient_Descent.py
@@ -1,1 +1,4 @@
 #We won't exactly be implementing Gradient descent in ML pursuits but we're learning GD rn to understand the internals of ML
+#Mean square erro is also called cost function
+
+#MSE = 1/n sigma (yi - (mxi+b))^2

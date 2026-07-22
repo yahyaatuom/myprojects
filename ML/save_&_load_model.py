07885e430bdc9b2ba455f7cbd96ae70be099d6c2
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 21:48:30 +0400

    instructions

--- ML/save_&_load_model.py
+++ ML/save_&_load_model.py
@@ -1,1 +1,8 @@
 #Save and load trained model using, 1. pickle , 2. sklearn joblib
+
+#Solving a problem with ML usually consists of 2 steps
+#1. Training  the Model, using the training dataset
+# 2. asking the trained model some questions
+
+# Often our Datasets are HUGE!!!!! reuiring GB's of data, for that we load already trained model
+ 

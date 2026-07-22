a4a2fec840ce6860d6183346aa63062848c5376d
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 21:53:19 +0400

    Model is stored

--- ML/home_price_predictor.py
+++ ML/home_price_predictor.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model  # The most important library here
 
-df = pd.read_csv("houses.csv", sep="\t")
+df = pd.read_csv("ML/houses.csv", sep="\t")
 #print(df)
 
 # plt.scatter(df.area, df.price, color = "red", marker = "+")
@@ -27,10 +27,14 @@ plt.xlabel('area', fontsize=20)
 plt.ylabel('price',fontsize=20)
 plt.scatter(df.area,df.price,color='red',marker='+')
 plt.plot(df.area, reg.predict(df[['area']]), color = 'blue')
-plt.show()
+#plt.show()
 # d = pd.read_csv("area.csv")
 # d.head(3)
 
 # p = reg.predict(d)
 # d['prices'] = p
 # d.to_csv('prediction.csv',index=False)
+
+import pickle
+with open('model_pickle','wb') as f:
+    pickle.dump(reg, f)

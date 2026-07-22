f71f12b937cd1e9f3ccbfb1ebfef02047b88426f
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 14:09:42 +0400

    Na exp. filled with zero

--- ML/HR_Form.py
+++ ML/HR_Form.py
@@ -6,4 +6,9 @@ from sklearn import linear_model
 from word2number import w2n
 
 df = pd.read_csv("ML/hiring.csv")
+#print(df)
+
+import math
+
+df.experience = df.experience.fillna("zero")
 print(df)

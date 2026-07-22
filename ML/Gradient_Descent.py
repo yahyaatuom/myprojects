ca33c40af76d4300796567a9d26b68314fd1c718
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 15:33:29 +0400

    GD

--- ML/Gradient_Descent.py
+++ ML/Gradient_Descent.py
@@ -2,3 +2,5 @@
 #Mean square erro is also called cost function
 
 #MSE = 1/n sigma (yi - (mxi+b))^2
+#We cannot take each and every line in a massive dataset (not a smart approach), hence we use grdient descent
+#GD is an algorithm that finds best fit line for given training data set

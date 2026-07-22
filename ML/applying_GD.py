a0a2cfc7ef34f52e39c0ac7debba93e2eb7b430b
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 20:38:27 +0400

    conitnuing..

--- ML/applying_GD.py
+++ ML/applying_GD.py
@@ -7,3 +7,6 @@ print(df)
 x = df['maths'].values
 y=df['cs'].values
 
+#feature scaling, scaling the data so learning rate doesn't cause divergence
+x_mean, x_std = np.mean(x), np.std(x)
+y_mean, y_std = np.mean(y), np.std(y)

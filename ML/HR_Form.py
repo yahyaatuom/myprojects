4b642dfe77242bed931360586e6b5bf3ce8a1ef8
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-22 14:04:24 +0400

    Modules imported

--- ML/HR_Form.py
+++ ML/HR_Form.py
@@ -1,1 +1,6 @@
 #Training a model for the HR Dept. Using LR Algorithm with multiple variables, to predict salary for an emp based on some factors
+
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from word2number import w2n

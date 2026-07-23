67e7d6a8f0a539fde4ad43383442ccc5d809fb68
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-23 15:25:46 +0400

    removed comments and rewrote using an LLM

--- ML/dummy_var.py
+++ ML/dummy_var.py
@@ -1,51 +1,27 @@
-#One Hot Encoding & Dummy Variables
-
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 df = pd.read_csv("ML/homeprices.csv")
-# print(df)
 
 dummies = pd.get_dummies(df.town)
-#print(dummies)
+merged = pd.concat([df, dummies], axis='columns')
+final = merged.drop(['town', 'west windsor'], axis='columns')
 
-merged = pd.concat([df,dummies],axis='columns')
-#print(merged)
-
-final = merged.drop(['town','west windsor'], axis='columns')
-#print(final)
-
-
-from sklearn.linear_model import LinearRegression
 model = LinearRegression()
-
-x = final.drop('price',axis='columns')
+x = final.drop('price', axis='columns')
 y = final.price
+model.fit(x, y)
 
-model.fit(x,y)
-
-# h=model.predict([[2000,0,0]])
-# print(h)
-
-# e = model.score(x,y)
-# print(e)
-
-from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
-
-dfle = df
+dfle = df.copy()
 dfle.town = le.fit_transform(dfle.town)
 
-X = dfle[['town','area']].values
-#print(X)
+X = dfle[['town', 'area']].values
+Y = dfle[['town', 'area', 'price']].values
 
-Y = dfle.values
-#print(Y)
-
-from sklearn.preprocessing import OneHotEncoder
 ohe = OneHotEncoder(sparse_output=False)
-
-#This assumes that X are cateforical variables
-# ohe.fit_transform(X)
-
 X_encoded = ohe.fit_transform(X)
 print(X_encoded)

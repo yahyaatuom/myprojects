4299b0e3e8e0d5c44541bde7d2bcb1ef60eb1bd5
yahyaatuom <yahya.khan@uom.edu.pk>
2026-07-24 22:41:21 +0400

    will continue later

--- NLI.py
+++ NLI.py
@@ -1,1 +1,7 @@
 #Using a NLI model (MoitzLaurer)
+
+from pydantic.v1 import BaseModel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")

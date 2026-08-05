8c5e517fc857288b04198ce416456acdf60c84e7
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-05 21:37:44 +0400

    Model trained and model scored

--- ML/home_price_predictor.py
+++ ML/home_price_predictor.py
@@ -1,48 +1,15 @@
-#Simple Linear Regression model that'd predict home prices in BatKhela, KPK, Pakistan
-
-import pandas as pd
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import linear_model  # The most important library here
 
-df = pd.read_csv("ML/houses.csv", sep="\t")
-#print(df)
+diabetes = load_diabetes()
+X, y = diabetes.data, diabetes.target
 
-# plt.scatter(df.area, df.price, color = "red", marker = "+")
-# plt.xlabel("Area (Sq.Ft)")
-# plt.ylabel("Price(PKR)")
+# Linear regression with cross-validation
+lr = LinearRegression()
+scores = cross_val_score(lr, X, y, cv=5)
 
-# #Training the Model
-
-reg = linear_model.LinearRegression()
-reg.fit(df[["area"]], df.price)
-
-# print(reg.predict([[50000]]))
-
-#print(reg.coef_)
-#print(reg.intercept_)
-
-#print(7481.08108108*5000+-6867837.837837841)
-plt.xlabel('area', fontsize=20)
-plt.ylabel('price',fontsize=20)
-plt.scatter(df.area,df.price,color='red',marker='+')
-plt.plot(df.area, reg.predict(df[['area']]), color = 'blue')
-#plt.show()
-# d = pd.read_csv("area.csv")
-# d.head(3)
-
-# p = reg.predict(d)
-# d['prices'] = p
-# d.to_csv('prediction.csv',index=False)
-
-import pickle
-with open('model_pickle','wb') as f:
-    pickle.dump(reg, f)
-
-print("Success! Model trained and saved as model_pickle.")
-
-with open('model_pickle','rb') as f:
-    mp = pickle.load(f)
-
-a = mp.predict([[4000]])
-print(a)
+print(f"R² scores: {scores}")
+print(f"Mean R²: {scores.mean():.3f}")
+print(f"Feature names: {diabetes.feature_names}")

5b53055b6e2382806a2035d39de95c9e18f6b2d1
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-11 14:06:25 +0400

    copying tensor structures

--- pytorch.py
+++ pytorch.py
@@ -1,5 +1,19 @@
 import torch
-data = [[1,2,3],[4,5,6],[6,7,8],[0,1,2]]
-my_tensor = torch.tensor(data)
+# data = [[1,2,3],[4,5,6],[6,7,8],[0,1,2]]
+# my_tensor = torch.tensor(data)
 
-print(my_tensor)
+# print(my_tensor)
+
+#shape = (2,3)
+
+#ones = torch.ones(shape)
+#zeros = torch.zeros(shape)
+#random=torch.randn(shape)
+
+#print(f'Random Tensor:\n {random}')
+
+template = torch.tensor([[1,2],[3,4]])
+rand_like = torch.randn_like(template,dtype=torch.float)
+
+print(f'Template Tensor:\n {template}')
+print(f'Randn_like Tensor:\n {rand_like}')

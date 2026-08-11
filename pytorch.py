f36fff0aef0c55470b160f3b2ac87a5cd4066109
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-11 15:08:12 +0400

    Matrix multiplication

--- pytorch.py
+++ pytorch.py
@@ -29,13 +29,19 @@ w = torch.tensor([[1.0],[2.0]],requires_grad=True) #Parameter
 # print(f'Data tensor requires_grad: {x_data.requires_grad}')
 # print(f'Parameter tensor requires_grad: {w.requires_grad}')
 
-a = torch.tensor(2.0,requires_grad=True)
-b = torch.tensor(3.0,requires_grad=True)
-x = torch.tensor(4.0,requires_grad=True)
+# a = torch.tensor(2.0,requires_grad=True)
+# b = torch.tensor(3.0,requires_grad=True)
+# x = torch.tensor(4.0,requires_grad=True)
 
-y = a+b
-z = x*y
+# y = a+b
+# z = x*y
 
-print(f"grad_fn for z: {z.grad_fn}")
-print(f"grad_fn for y: {y.grad_fn}")
-print(f"grad_fn for a: {a.grad_fn}")
+# print(f"grad_fn for z: {z.grad_fn}")
+# print(f"grad_fn for y: {y.grad_fn}")
+# print(f"grad_fn for a: {a.grad_fn}")
+
+a = torch.tensor([[1,2,3],[4,5,6]])
+b = torch.tensor([[7,8],[9,10],[11,12]])
+
+element_wise_product = a@b
+print(element_wise_product)

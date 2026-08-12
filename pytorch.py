ae4c50448fddcb77ededb8212e4533d23a3836b5
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-12 14:25:13 +0400

    Backward propagation

--- pytorch.py
+++ pytorch.py
@@ -1,72 +1,30 @@
 import torch
-# data = [[1,2,3],[4,5,6],[6,7,8],[0,1,2]]
-# my_tensor = torch.tensor(data)
+#Linear regression y^ = XW+b
 
-# print(my_tensor)
+N = 10
+D_in = 1
+D_out = 1
 
-#shape = (2,3)
+X = torch.randn(N,D_in)
 
-#ones = torch.ones(shape)
-#zeros = torch.zeros(shape)
-#random=torch.randn(shape)
+true_W = torch.tensor([[2.0]])
+true_b = torch.tensor(1.0)
+y_true = X @ true_W + true_b + torch.randn(N,D_out)*0.1
 
-#print(f'Random Tensor:\n {random}')
+W = torch.randn(D_in,D_out,requires_grad=True)
+b = torch.randn(1,requires_grad=True)
 
-# template = torch.tensor([[1,2],[3,4]])
-# rand_like = torch.randn_like(template,dtype=torch.float)
+# print(f"Initial Weight W:\n {W}\n")
+# print(f"Initial Bias b:\n {b}")
 
-# print(f'Template Tensor:\n {template}')
-# print(f'Randn_like Tensor:\n {rand_like}')
+y_hat = X @ W + b
+# print(y_hat[:3])
+# print(y_true[:3])
 
-# tensor = torch.randn(2,3)
-# print(f"shape: {tensor.shape}")
-# print(f"device: {tensor.device}")
-# print(f"dtype: {tensor.dtype}")
+#calculate loss
+error = y_hat - y_true
+squared_error = error**2
+loss = squared_error.mean()
 
-x_data = torch.tensor([[1.,2.],[3.,4.]]) #data
-w = torch.tensor([[1.0],[2.0]],requires_grad=True) #Parameter
-
-# print(f'Data tensor requires_grad: {x_data.requires_grad}')
-# print(f'Parameter tensor requires_grad: {w.requires_grad}')
-
-# a = torch.tensor(2.0,requires_grad=True)
-# b = torch.tensor(3.0,requires_grad=True)
-# x = torch.tensor(4.0,requires_grad=True)
-
-# y = a+b
-# z = x*y
-
-# print(f"grad_fn for z: {z.grad_fn}")
-# print(f"grad_fn for y: {y.grad_fn}")
-# print(f"grad_fn for a: {a.grad_fn}")
-
-# a = torch.tensor([[1,2,3],[4,5,6]])
-# b = torch.tensor([[7,8],[9,10],[11,12]])
-
-# element_wise_product = a@b
-# print(element_wise_product)
-
-# scores = torch.tensor([[10.,20.,30.],[5.,10.,15.]])
-# average_per_assg = scores.mean(dim=0)
-# avg_per_std=scores.mean(dim=1)
-# print(f"average per assignment: {average_per_assg}")
-# print(f"average per student: {avg_per_std}")
-
-# x = torch.arange(12).reshape(3,4)
-# print(x)
-
-# scores = torch.tensor([
-#     [10,0,5,20,1],
-#     [1,30,2,5,0]
-# ])
-# print(torch.argmax(scores,dim=1))
-
-data = torch.tensor([
-    [10,11,12,13,],
-    [20,21,22,23],
-    [30,31,32,33]
-])
-
-indices_to_select = torch.tensor([[2],[0],[3]])
-selected_values = torch.gather(data, dim=1,index=indices_to_select)
-print(selected_values)
+# print(f'Loss (our single scorecard number): {loss}')
+loss.backward()

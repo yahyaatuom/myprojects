d1bbeb2b3ab2223a3fc64e1dafe5fbc518bc909f
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-12 19:34:04 +0400

    softmax

--- pytorch.py
+++ pytorch.py
@@ -1,8 +1,8 @@
 import torch
 
-relu = torch.nn.ReLU()
-sample_data = torch.tensor([-2.0,-0.5,0.0,0.5,2.0])
-activated_data = relu(sample_data)
+soft = torch.nn.Softmax(dim=-1)
+logits = torch.tensor([[1.0,3.0,0.5,1.5],[-1.0,2.0,1.0,0.0]])
+probabilities = soft(logits)
 
-print(f'Original Data:   {sample_data}')
-print(f'Data after ReLU: {activated_data}')
+print(f'Output Probabilities:\n   {probabilities}\n')
+print(f'Sum of probabilites for item 1: {probabilities[0].sum()}')

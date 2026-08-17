9a2d4ee41d8e4c56b3a8763663fcbda264ee5a7c
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-17 14:13:36 +0400

    done

--- ML/knn.py
+++ ML/knn.py
@@ -1,7 +1,27 @@
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 from sklearn.datasets import load_iris
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+
 iris = load_iris()
 
 df = pd.DataFrame(iris.data,columns=iris.feature_names)
-print(df.head)
+df['target'] = iris.target
+#print(df.head)
+
+
+X = df.drop(['target'],axis='columns')
+y = df.target
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3)
+
+knn = KNeighborsClassifier(n_neighbors = 8)
+knn.fit(X_train,y_train)
+score = knn.score(X_test,y_test)
+print(score)
+
+y_pred = knn.predict(X_test)
+cm = confusion_matrix(y_test,y_pred)
+print(cm)

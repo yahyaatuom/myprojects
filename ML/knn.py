7f268de66ed49b1253d9b27dfb1b1b760f563163
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-17 14:07:37 +0400

    Libraries and data imported

---
+++ ML/knn.py
@@ -0,0 +1,7 @@
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+df = pd.DataFrame(iris.data,columns=iris.feature_names)
+print(df.head)

0e8805a2c37fc05bfdb775107b8e9271251fedcf
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-27 16:08:16 +0400

    Trying other algorithms, which dont seem to be working very well

--- ML/model.py
+++ ML/model.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVC
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
@@ -11,24 +11,22 @@ y = energy_scores
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
-model = LinearRegression()
+model = SVC()
 model.fit(X_train,y_train)
 
 predictions = model.predict(X_test)
 mse = mean_squared_error(y_test,predictions)
-# print(f"Predicted scores:  {predictions}")
-# print(f"Actual Score: ", y_test)
-# print(f"Mean Squared Error: {mse}")
+print(f"Predicted scores:  {predictions}")
+print(f"Actual Score: ", y_test)
+print(f"Mean Squared Error: {mse}")
 
 score = model.score(X_test,y_test)
 print(f"Model prediction accuracy: {score}")
 
-# plt.scatter(X,y,color="blue",label= "Real data")
-# plt.plot(X, model.predict(X),color = "green", label= "Model Prediction")
-# plt.xlabel("Hours Slept")
-# plt.ylabel("Energy scores")
-# plt.title("Hours Slept vs Energy Scores")
-# plt.legend()
-# plt.show()
-
-print(model.predict([[100]]))
+plt.scatter(X,y,color="blue",label= "Real data")
+plt.plot(X, model.predict(X),color = "green", label= "Model Prediction")
+plt.xlabel("Hours Slept")
+plt.ylabel("Energy scores")
+plt.title("Hours Slept vs Energy Scores")
+plt.legend()
+plt.show()

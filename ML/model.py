4a2668d19f9cd153995099e664899cb8d9c7fc1f
yahyaatuom <yahya.khan@uom.edu.pk>
2026-08-27 15:59:51 +0400

    New

---
+++ ML/model.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+hours_slept = [4,5,5.5,6,6.5,7,7.5,8,8.5,9]
+energy_scores = [32,38,41,45,47,49,58,67,78,90]
+
+X = [[hours] for hours in hours_slept]
+y = energy_scores
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+
+model = LinearRegression()
+model.fit(X_train,y_train)
+
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test,predictions)
+print(f"Predicted scores:  {predictions}")
+print(f"Actual Score: ", y_test)
+print(f"Mean Squared Error: {mse}")

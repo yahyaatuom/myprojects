57409aa2a5d81fea95d577399bfd879e618ffbc1
yahyaatuom <yahya.khan@uom.edu.pk>
2026-09-03 22:38:13 +0400

    Fix

--- check.py
+++ check.py
@@ -1,6 +1,7 @@
 #Lets see how to import a function from another program in the same directory
 from height import feet_to_inch
 
+
 height = float(input("Enter your height in feet: "))
 if height <= 0:
     print("Height cannot be 0 or a negative value")

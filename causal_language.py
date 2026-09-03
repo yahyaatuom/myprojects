16f82a7fd72c88a5bd9ed320d85acaa545201c7a
yahyaatuom <yahya.khan@uom.edu.pk>
2026-09-03 22:44:04 +0400

    done

--- causal_language.py
+++ causal_language.py
@@ -1,10 +1,7 @@
 vocab = ["because", "caused","led to","lead to","resulted in",'concluded']
 dict = {}
-length = len(vocab)
-for words in vocab:
-    n = 0
-    dict.append(vocab[n])
-    while len(dict)<n:
-        n = n+1
+
+for i,word in enumerate(vocab):
+    dict[word] = i
 
 print(dict)

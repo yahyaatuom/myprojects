7464c38826bcee75d87de4767fa0ea20c104fee1
yahyaatuom <yahya.khan@uom.edu.pk>
2026-09-03 22:42:04 +0400

    wrong:

---
+++ causal_language.py
@@ -0,0 +1,10 @@
+vocab = ["because", "caused","led to","lead to","resulted in",'concluded']
+dict = {}
+length = len(vocab)
+for words in vocab:
+    n = 0
+    dict.append(vocab[n])
+    while len(dict)<n:
+        n = n+1
+
+print(dict)

cc6585380fc9d3adac112b43b9c30a5a57b4d4be
yahyaatuom <yahya.khan@uom.edu.pk>
2026-09-08 07:54:45 +0400

    Data imported

---
+++ ML/digits_rf.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_digits
+
+digits = load_digits()

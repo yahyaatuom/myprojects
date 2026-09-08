8298e6dc15d373959cf7063eb90cf0bcc72ce6e4
yahyaatuom <yahya.khan@uom.edu.pk>
2026-09-08 16:03:34 +0400

    done

--- ML/digits_rf.py
+++ ML/digits_rf.py
@@ -4,3 +4,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_digits
 
 digits = load_digits()
+df = pd.DataFrame(digits.data)
+
+df['target'] = digits.target
+
+X_train,X_test,y_train,y_test = train_test_split(df.drop(['target'],axis='columns'),digits.target,test_size=0.2)
+
+model = RandomForestClassifier()
+model.fit(X_train,y_train)
+a = model.score(X_test,y_test)
+print(a)

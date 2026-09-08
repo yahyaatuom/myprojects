bcc5e58b026c58e463a5d1223f3ac99e0e85204e
yahyaatuom <yahya.khan@uom.edu.pk>
2026-09-08 16:04:49 +0400

    Library for cm

--- ML/digits_rf.py
+++ ML/digits_rf.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 digits = load_digits()
 df = pd.DataFrame(digits.data)
@@ -14,3 +16,5 @@ model = RandomForestClassifier()
 model.fit(X_train,y_train)
 a = model.score(X_test,y_test)
 print(a)
+
+#-- Confusion Matrix
